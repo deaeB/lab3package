cde8f2f9b9157ea64e071c378925b125f60fbb56
NI D.W <deaeb@hotmail.com>
2022-09-15 04:41:20 +0200

    1.2.4 Include the dataset wiki graph in the package

---
+++ R/Documentation_wiki_graph.R
@@ -0,0 +1,15 @@
+#' wiki_graph for 1.1.2 * dijkstra()
+#' 
+#' a data.frame of a graph with three variables
+#' 
+#' @format A data frame with 3 cols and 18 rows:
+#' \describe{
+#'  \item{v1, v2}{the edges of the graph}
+#'  \item{w}{the weight of the edge}
+#' }
+#' @references https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+#' @seealso https://en.wikipedia.org/wiki/Graph_(mathematics)
+#' 
+
+## code to prepare `wiki_graph` dataset goes here
+"wiki_graph"
